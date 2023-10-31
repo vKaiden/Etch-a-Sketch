85e204c44b207312ba4dea70d48162ea9c3969e2
Kaiden Taylor <kaidentaylor502@gmail.com>
2023-10-30 20:03:57 -0500

    createBoard function and some CSS

--- Etch-a-Sketch-Project-TOP/script.js
+++ Etch-a-Sketch-Project-TOP/script.js
@@ -1,13 +1,20 @@
+
 const SetGridButton = document.querySelector('.popup')
 const container = document.querySelector('.container')
-const board = document.querySelector('.board')
+let board = document.querySelector('.board')
 const randomBtn = document.querySelector('.randomBtn');
 const resetBtn = document.querySelector('.resetBtn')
 
 function createBoard(size) {
-  board.style.gridTemplateRows = repeat(size, 1);
-  board.style.gridTemplateColumns = repeat(size, 1);
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+let numDivs = size * size;
+
+for(let i = 0; i < numDivs; i++) {
+ let div = document.createElement('div');
+  div.style.backgroundColor = "yellow"
+  board.insertAdjacentElement("beforeend", div)
 }
 
-
-
+}
