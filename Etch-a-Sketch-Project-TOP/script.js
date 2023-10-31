a93bf1c56f18126da299d562914c28d6ca044786
Kaiden Taylor <kaidentaylor502@gmail.com>
2023-10-30 19:21:18 -0500

    Simple CSS Styling

--- Etch-a-Sketch-Project-TOP/script.js
+++ Etch-a-Sketch-Project-TOP/script.js
@@ -1,6 +1,8 @@
-const SetGridButton = document.querySelector('.setGrid')
+const SetGridButton = document.querySelector('.popup')
 const container = document.querySelector('.container')
 const board = document.querySelector('.board')
+const randomBtn = document.querySelector('.randomBtn');
+const resetBtn = document.querySelector('.resetBtn')
 
 function createBoard(size) {
   board.style.gridTemplateRows = repeat(size, 1);
